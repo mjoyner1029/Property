28de6c748a411f725bad9c2e5f5b7cd6a68ebb82
mjoyner1029 <mjoyner18@gmail.com>
2025-06-25 23:45:27 -0700

    new files

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,14 @@
+// frontend/src/utils/socket.js
 
-import { io } from "socket.io-client";
-const socket = io(import.meta.env.VITE_BACKEND_URL || "http://localhost:5050", {
-  transports: ["websocket"],
+import { io } from 'socket.io-client';
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
+
+const socket = io(backendUrl, {
+  transports: ['websocket'],
+  withCredentials: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
 });
+
 export default socket;
