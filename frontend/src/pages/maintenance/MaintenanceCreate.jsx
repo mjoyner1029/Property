5d234c9c542fab1a176afc469e52d054e0df5eee
mjoyner-sp <mjoyner@soundthinking.com>
2025-09-01 21:13:36 -0700

    test(maintenance): add MaintenanceCreate component and minimal test

---
+++ frontend/src/pages/maintenance/MaintenanceCreate.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MaintenanceCreate() {
+  return (
+    <div data-testid="maintenance-create">
+      <h1>Create Maintenance Request</h1>
+    </div>
+  );
+}
